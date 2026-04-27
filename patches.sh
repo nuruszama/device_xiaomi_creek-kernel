99c4bd67a5d849fa6c114a2fd45b18bef527eaa2
Abdul Samad K A <85991146+nuruszama@users.noreply.github.com>
2026-04-27 20:11:37 +0400

    Update patches.sh

--- patches.sh
+++ patches.sh
@@ -7,7 +7,7 @@ BASE="$(dirname "$0")/scripts"
 
 echo "[*] Running Creek patch pipeline..."
 
-bash "$BASE/01_qcom_defs.sh"
+# bash "$BASE/01_qcom_defs.sh"
 # bash "$BASE/02_qcom_defaults.sh"
 # bash "$BASE/03_display_patch.sh"
 # bash "$BASE/04_audio_patch.sh"
